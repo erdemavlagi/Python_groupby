218cd3afde539215248323a77c9360a5bea7945a
Erdem <erdemavlagi@gmail.com>
2023-09-23 17:42:46 +0300

    enumerate

--- setting.py
+++ setting.py
@@ -1,17 +1,31 @@
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+
+df[df["age"] > 50] ["age"].count()
+
+df.loc[df["age"] > 50 , ["class","age"]].head()
+
+df.loc[(df["age"] > 50) & (df["sex"] == "male") , ["class","age"]].head()
+
+df.loc[(df["age"] > 50)
+       & (df["sex"] == "male")
+       & (df["embark_town"] == "Southampton"), ["class","age","embark_town"]].head()
 
 
-ogrenciler =  [ "jonh", "mary", "erdem", "esra", "gizem","avlağı"]
-
-A = []
-B = []
-
-for index, ogrenci in enumerate(ogrenciler):
-    if index % 2 == 0:
-        A.append(ogrenci)
-    else:
-        B.append(ogrenci)
 
 
-print("A Grubu ",A)
 
-print("B grubu ", B)
+df["age"].mean()
+df["age"].count()
+df["age"].min()
+df["age"]
+
+
+df.groupby("sex")["age"].mean()
+
+df.groupby("sex").agg({"age": "mean"})
+
+df.groupby("sex").agg({"survived": "mean"})
